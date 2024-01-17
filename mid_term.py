9d0aae5f8ddb831f1cd571fb8d5ba99c3e787d6c
50722 <156940277+50722@users.noreply.github.com>
2024-01-17 22:06:55 +0500

    Add files via upload

---
+++ mid_term.py
@@ -0,0 +1,21 @@
+# Hanzala
+# num 03203066496
+
+
+# Q1 - Write a Python program to do arithmetical operations addition and division
+
+num1=input("enter the first number:"number 50)
+num2=input("enter the second number:"number 20)
+sum=num1+num2
+print("the sum of num1 and num2)
+      division=num1/num2
+
+
+
+
+
+
+
+
+
+
---
+++ task2.py
@@ -0,0 +1,9 @@
+#Q4 - Write a Python program that return all datatypes that we discussed in the class.
+a=(2+2)
+print(a)
+print(type(a))
+
+s=(2.3)
+print(s)
+print(type(s))
+
